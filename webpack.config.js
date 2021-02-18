233608c6e000f4c0c5104e989a5d82a781d61c6c
gianicola <gianicolajara@gmail.com>
2021-02-17 21:16:01 -0400

    template webpack pug-sass-babel

---
+++ webpack.config.js
@@ -0,0 +1,73 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require("path");
+
+const babelRules = {
+  test: /\.m?js$/i,
+  exclude: /(node_modules|bower_components)/,
+  use: {
+    loader: "babel-loader",
+    options: {
+      presets: ["@babel/preset-env"],
+      cacheDirectory: true,
+    },
+  },
+};
+
+const pugRules = {
+  test: /\.pug$/,
+  use: [
+    {
+      loader: "pug-loader",
+    },
+  ],
+};
+
+const fileRules = {
+  test: /\.(png|jpe?g|gif|svg)$/i,
+  use: [
+    {
+      loader: "file-loader",
+      options: {
+        name: "[contenthash].[ext]",
+        outputPath: "./assets/images",
+      },
+    },
+    {
+      loader: "image-webpack-loader",
+    },
+  ],
+};
+
+const sassRules = {
+  test: /\.s[ac]ss$/i,
+  use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+};
+
+module.exports = {
+  entry: {
+    index: "./src/index.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.pug",
+      filename: "./index.html",
+      chunks: ["index"],
+      hash: true,
+    }),
+    new MiniCssExtractPlugin({
+      filename: "css/main.css",
+    }),
+    new CleanWebpackPlugin({
+      cleanStaleWebpackAssets: true,
+    }),
+  ],
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "js/[name].[chunkhash].js",
+  },
+  module: {
+    rules: [babelRules, fileRules, pugRules, sassRules],
+  },
+};
